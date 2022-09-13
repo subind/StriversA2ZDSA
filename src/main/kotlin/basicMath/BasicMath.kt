a9449472235b08af2199fc919c13f9876a8a2440
VSUBISU <subind.subind@pearson.com>
2022-09-13 06:13:20 +0530

    Basic math : Reverse a number

--- src/main/kotlin/basicMath/BasicMath.kt
+++ src/main/kotlin/basicMath/BasicMath.kt
@@ -4,6 +4,9 @@ import simulator
 
 fun main() {
 
+    /**
+     * Count digits
+     */
     simulator("Count digits in a number solution 1") {
         val inputString = "12345"
         println(inputString.length)
@@ -17,11 +20,26 @@ fun main() {
     simulator("Count digits in a number solution 3") {
         var x: Int = 12345
         var counter = 0
-        while(x != 0) {
+        while (x != 0) {
             x = x / 10
             ++counter
             println("count is $counter, value of x is $x")
         }
+    }
+
+    /**
+     * Reverse a number
+     */
+    simulator("Reverse a number solution 1") {
+        var input = 123
+        var tempOutput = ""
+
+        while (input > 0) {
+            tempOutput += (input % 10).toString()
+            input /= 10
+        }
+
+        println(tempOutput)
     }
 
 
