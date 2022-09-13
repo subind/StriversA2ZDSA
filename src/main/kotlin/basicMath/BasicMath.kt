1c3a4b4858bdd411e5fc06c9b677baf1d6da28b6
VSUBISU <subind.subind@pearson.com>
2022-09-13 06:43:22 +0530

    Basic math: Reverse a number, returns reversed number instead of string

--- src/main/kotlin/basicMath/BasicMath.kt
+++ src/main/kotlin/basicMath/BasicMath.kt
@@ -30,16 +30,30 @@ fun main() {
     /**
      * Reverse a number
      */
+    /**
+     *  given input integer
+     *  using modulo operator % with 10 we can get the right most integer
+     *  using divide operator / with 10 we can exclude the right most integer & get the rest to its left
+     *  the entire process should take place inside the while loop, as long as the integer getting reduced by division with 10 is greater than 0
+     *  the right most integer should be stored in a temporary variable, & at each step
+     *  this mentioned temporary variable must be multiplied with 10 before adding the new right most variable which is the latest result of modulo 10 operation
+     *  for the above step, if the temp is greater than 1, multiply it with 10, else skip the multiplication step
+     */
     simulator("Reverse a number solution 1") {
-        var input = 123
-        var tempOutput = ""
+        var input = 123456789
+        var temp = 0
+        var output = 0
 
         while (input > 0) {
-            tempOutput += (input % 10).toString()
+            temp = input % 10
             input /= 10
+            if(output > 0){
+                output *= 10
+            }
+            output += temp
         }
 
-        println(tempOutput)
+        println(output)
     }
 
 
