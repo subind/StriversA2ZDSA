e41e968638942bf767c5f8f8feaddb5384e2c539
VSUBISU <subind.subind@pearson.com>
2022-10-04 08:29:43 +0530

    sumOfFirstNNumbers

--- src/main/kotlin/basicRecursion/Recursion.kt
+++ src/main/kotlin/basicRecursion/Recursion.kt
@@ -7,18 +7,52 @@ fun main() {
     /**
      * Understand recursion by printing something N times
      */
-    /**
-     * A function which calls itself, but has a base condition within which stops itself at some point.
-     * fun name 'recursiveFun', input integer 'n' which is the number of times it should print,
-     *
-     */
     simulator("Understand recursion by print something N times") {
         val input = 5
         val temp = 1
         recursiveFunction(input, temp)
     }
 
+    /**
+     * Print 1 to n using recursion
+     */
+    simulator("Print 1 to n using recursion") {
+        val n = 10
+        val controller = 1
+        oneToNRecursively(n, controller)
 
+    }
+
+    /**
+     * Sum of first N numbers
+     */
+    simulator("Sum of first N numbers") {
+        sumOfFirstNNumbers(5, 0, 0)
+    }
+
+}
+
+fun sumOfFirstNNumbers(n: Int, presentSum: Int, tempController: Int) {
+    var presentSum = presentSum
+    var tempController = tempController
+
+    ++tempController
+    if(tempController > n) {
+        println(presentSum)
+        return
+    }
+    presentSum += tempController
+    sumOfFirstNNumbers(n, presentSum, tempController)
+}
+
+fun oneToNRecursively(n: Int, controller: Int) {
+    var controller = controller
+    if(controller > n) {
+        return
+    }
+    println(controller)
+    ++controller
+    oneToNRecursively(n, controller)
 }
 
 fun recursiveFunction(input: Int, temp: Int) {
