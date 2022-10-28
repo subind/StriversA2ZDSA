558a366f62112472909d25f3fbec338785700b8e
VSUBISU <subind.subind@pearson.com>
2022-10-28 21:47:06 +0530

    Selection sort

--- src/main/kotlin/sortingTechniques/sorting1/Sorting1.kt
+++ src/main/kotlin/sortingTechniques/sorting1/Sorting1.kt
@@ -57,4 +57,49 @@ fun main() {
         println(unSortedList)
     }
 
+    /**
+     * Selection sort :-
+     * arrowPos starts at the very beginning, i.e, at index 0
+     * 'arrowPos' gets incremented by 1 at the end of every iteration
+     * 'smallestElement' is the variable to store the smallest element found in an iteration, also store the corresponding location in 'smallestElementPos'.
+     * Compare element at 'arrowPos' with element at 'smallestElement', if 'smallestElement' is smaller, then swap them both. Use 'tempStorage' variable for intermediate operation.
+     */
+    /**
+     * Two loops
+     * Outer loop for iterating length-1 times to sort the entire list
+     * Inner loop for progressing arrowPos from one element to the next
+     * if statement: 'inner loop index' > 'arrowPos'
+     * Store 'inner loop index' element in 'smallestElement' variable
+     * if statement: 'inner loop index' element < 'smallestElement', store 'inner loop index' in 'smallestElement', & also store 'inner loop index' pos in 'smallestElementPos'.
+     * After the end of inner loop, inc 'arrowPos' by 1.
+     * If statement: element at 'arrowPos' > 'smallestElement', swap them using 'tempStorage' as the intermediate storage location.
+     */
+    simulator("Selection sort") {
+        val unSortedList = mutableListOf(13, 46, 24, 52, 20, 9)
+        var arrowPos = 0
+        var smallestElement = 0
+        var smallestElementPos = 0
+        var tempStorage = 0
+
+        for(x in 1 until unSortedList.size) {
+            smallestElement = unSortedList[arrowPos + 1]
+            unSortedList.forEachIndexed { innerLoopIndex, innerLoopValue ->
+                if(innerLoopIndex > arrowPos) {
+                    if(innerLoopValue <= smallestElement) {
+                        smallestElement = innerLoopValue
+                        smallestElementPos = innerLoopIndex
+                    }
+                }
+            }
+            if(unSortedList[arrowPos] > smallestElement) {
+                tempStorage = unSortedList[arrowPos]
+                unSortedList[arrowPos] = smallestElement
+                unSortedList[smallestElementPos] = tempStorage
+            }
+            arrowPos++
+        }
+
+        println(unSortedList)
+    }
+
 }
